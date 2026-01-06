a55bf7d0d6ccaaca815de7a9c573e8eb95cc69f6
Swopnil Sapkota <swopnilsapkota7@gmail.com>
2026-01-06 13:39:15 +0545

    Bug Fixed for Production

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,7 +1,7 @@
 
 
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 import Footer from "../components/footer";
 
 const MainLayout = () => {
