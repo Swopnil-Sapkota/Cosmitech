9b1fd6e125cd9b8a71be4aee2a133956e7151d1a
Pranish97 <pranishstha4@gmail.com>
2026-01-06 14:04:57 +0545

    Added Design for explore category for fcl

--- src/pages/FclPage.jsx
+++ src/pages/FclPage.jsx
@@ -1,9 +1,11 @@
+import FCLExploreCategories from "../components/fcl/ExploreCategory";
 import FclHeroSection from "../components/fcl/HeroSection";
 
 const FclPage = () => {
   return (
     <div>
         <FclHeroSection/>
+        <FCLExploreCategories />
     </div>
   );
 };
