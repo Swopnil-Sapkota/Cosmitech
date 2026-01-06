7aabdd94add070c72db279497d1e7a4f16282fec
Swopnil Sapkota <swopnilsapkota7@gmail.com>
2026-01-06 13:23:43 +0545

    Fix Navbar import case sensitivity for Vercel

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,7 +1,7 @@
 
 
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 
 const MainLayout = () => {
