c8e5601ddae9e23406c1b29e53a1d631a9528ebb
sampanna17 <sampannapiya13@gmail.com>
2026-01-06 15:34:07 +0545

    Why Choose Us compoenent

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,14 @@
 import AboutCosmetich from "../components/about/AboutCosmetich.jsx";
 import EducationalFocus from "../components/about/EducationalFocus.jsx";
 import WhatWeDo  from "../components/about/WhatWeDo.jsx";
+import WhyFixdermaFCL from "../components/about/WhyFixdermaFCL.jsx";
 
 const AboutPage = () => {
   return (
     <div>
         <AboutCosmetich />
         <WhatWeDo  />
+        <WhyFixdermaFCL />
         <EducationalFocus />
     </div>
   );
