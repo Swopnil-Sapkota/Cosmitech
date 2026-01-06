f44f5a4f079a3c8a853c0e7f5380a77f6f44ea4c
Abhinav Shrestha <139767616+abhinavshr@users.noreply.github.com>
2026-01-06 12:41:03 +0545

    static design for the why to choose fixderma

--- src/pages/fixdermapage.jsx
+++ src/pages/fixdermapage.jsx
@@ -1,11 +1,13 @@
 import ExploreCategories from "../components/fixderma/ExploreCategory.jsx";
 import Herofixderma from "../components/fixderma/hero.jsx"
+import WhyChooseFixderma from "../components/fixderma/WhyChooseFixderma.jsx";
 
 const FixdermaPage = () => {
   return (
     <div>
         <Herofixderma />
         <ExploreCategories />
+        <WhyChooseFixderma />
     </div>
   );
 };
