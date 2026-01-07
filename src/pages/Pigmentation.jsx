f9dfe3e0672abd07a7f90dd337881af5e360f5bc
sampanna17 <sampannapiya13@gmail.com>
2026-01-07 17:03:31 +0545

    Pigmentation Info Page

--- src/pages/Pigmentation.jsx
+++ src/pages/Pigmentation.jsx
@@ -1,11 +1,14 @@
 import PigmentationAdvice from "../components/skinconcern/pigmentation/PigmentationAdvice";
 import PigmentationHeroSection from "../components/skinconcern/pigmentation/PigmentationHero";
+import PigmentationInfoSection from "../components/skinconcern/pigmentation/PigmentationInfoSection";
 
 const PigmentationPage = () => {
     return (
         <div className="w-full bg-white">
             <PigmentationHeroSection />
+            <PigmentationInfoSection />
             <PigmentationAdvice />
+            
         </div>
     );
 };
