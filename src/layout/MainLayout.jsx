d49eca9f6143af6aac456e1b693e0c3e3e425309
sampanna17 <sampannapiya13@gmail.com>
2026-01-07 13:38:07 +0545

    Scroll to top component

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,12 +2,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
+import ScrollToTop from "../components/ScrollToTop";
 
 const MainLayout = () => {
   return (
     <>
       <Navbar />
-       <main className="pt-16 md:pt-20 min-h-screen">
+      <ScrollToTop />
+      <main className="pt-16 md:pt-20 min-h-screen">
         <Outlet />
       </main>
       <Footer />
