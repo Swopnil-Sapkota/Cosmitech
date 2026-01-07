742a1d394072f5c29fae709667eafe7695474e4a
sampanna17 <sampannapiya13@gmail.com>
2026-01-07 17:14:52 +0545

    Pigmentation Product Component

--- src/pages/Pigmentation.jsx
+++ src/pages/Pigmentation.jsx
@@ -1,6 +1,7 @@
 import PigmentationAdvice from "../components/skinconcern/pigmentation/PigmentationAdvice";
 import PigmentationHeroSection from "../components/skinconcern/pigmentation/PigmentationHero";
 import PigmentationInfoSection from "../components/skinconcern/pigmentation/PigmentationInfoSection";
+import PigmentationProducts from "../components/skinconcern/pigmentation/PigmentationProducts";
 
 const PigmentationPage = () => {
     return (
@@ -8,7 +9,7 @@ const PigmentationPage = () => {
             <PigmentationHeroSection />
             <PigmentationInfoSection />
             <PigmentationAdvice />
-            
+            <PigmentationProducts />
         </div>
     );
 };
