2af091e046e799faa8034dcf4db78294c42950e2
Pranish97 <pranishstha4@gmail.com>
2026-01-07 11:17:57 +0545

    Added Design for body care and daily care section in body care page

--- src/pages/BodyCare.jsx
+++ src/pages/BodyCare.jsx
@@ -1,10 +1,27 @@
+import BodyCareCategories from "../components/fixderma/BodyCare/BodyCareCategories";
 import BodyCareHero from "../components/fixderma/BodyCare/Hero";
-
+import DailyCareRoutine from "../components/fixderma/FaceCare/DailyFaceCare";
 
 const BodyCare = () => {
   return (
-    <div>
-        <BodyCareHero  />
+    <div className="w-full bg-white">
+
+      {/* BREADCRUMB */}
+      <div className="max-w-7xl mx-auto px-4 pt-3">
+        <div className="text-base text-[#333333]">
+          <span className="hover:text-[#1D6B46] cursor-pointer">
+            Fixderma
+          </span>
+          <span className="mx-2">{">"}</span>
+          <span className="text-[#333333]">
+            Body care
+          </span>
+        </div>
+      </div>
+
+      <BodyCareHero />
+      <BodyCareCategories />
+      <DailyCareRoutine />
     </div>
   );
 };
